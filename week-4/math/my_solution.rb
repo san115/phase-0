5dee061af501b64789945deccadaae96d8b307dc
Un Choi <umo2587@gmail.com>
2015-09-11 12:14:52 -0400

    add math method files

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,22 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ add = num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ subtract = num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ multiply = num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ divide = num_1.to_f / num_2.to_f
+end
