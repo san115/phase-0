27a4218537456c3f95bb7812d36195e1032fc084
Un Choi <umo2587@gmail.com>
2015-09-27 17:20:28 -0400

    add gps changes

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -6,45 +6,45 @@
 
 # Bakery Serving Size portion calculator.
 
-def serving_size_calc(item_to_make, order_quantity)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+# def serving_size_calc(item_to_make, order_quantity)
+#   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
+#   error_counter = 3
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
-    end
-  end
-  # Doesn't seem like the if statement for library.each do does anything. It looks incorrect if meant to compare
+#   library.each do |food|
+#     if library[food] != library[item_to_make]
+#       p error_counter += -1
+#     end
+#   end
+#   # Doesn't seem like the if statement for library.each do does anything. It looks incorrect if meant to compare
   # the value in the library hash with item_to_make--need to declare value in variables for the each loop. In the 
   # end, this block of code just counts down the error_counter by increment of 1, but should be meant to calculate
   # for error? for when the key in the library (library[food]) does not match the item_to_make?
 
   
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
+  # if error_counter > 0
+  #   raise ArgumentError.new("#{item_to_make} is not a valid input")
+  # end
   # This section would stop the program and send an error message if there is an error--that the tiem_to_make 
   # doesn't match a key in the library hash.
  
-  serving_size = library.values_at(item_to_make)[0]
-  # The above line extracts the amount of serving size that is currently in the library as value for the 
-  # key(item_to_make) 
-  serving_size_mod = order_quantity % serving_size
-  # The above line calculates the serving size to be modified by figuring out how much ingredients would 
-  # be leftover when divide the quantity for the order by the existing amount of serving size
+#   serving_size = library.values_at(item_to_make)[0]
+#   # The above line extracts the amount of serving size that is currently in the library as value for the 
+#   # key(item_to_make) 
+#   serving_size_mod = order_quantity % serving_size
+#   # The above line calculates the serving size to be modified by figuring out how much ingredients would 
+#   # be leftover when divide the quantity for the order by the existing amount of serving size
 
-  case serving_size_mod
-  when 0
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
-  # The above sends notice when there is no leftover (serving_size_mod == 0) that there is enough ingredients to make
-  # the food item to make the order quantity
-  else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
-  # The above sends notice when there is leftover (serving_size_mod > 0) ingredients after making the order quantity
-  # and gives notice to do something else with the leftover 
-  end
-end
+#   case serving_size_mod
+#   when 0
+#     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+#   # The above sends notice when there is no leftover (serving_size_mod == 0) that there is enough ingredients to make
+#   # the food item to make the order quantity
+#   else
+#     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+#   # The above sends notice when there is leftover (serving_size_mod > 0) ingredients after making the order quantity
+#   # and gives notice to do something else with the leftover 
+#   end
+# end
 
 # Refactored:
 
@@ -54,28 +54,23 @@ end
    
 def serving_size_calc(item_to_make, order_quantity)
   stock = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 2
-
-   stock.each_key do |food|  
-    if stock[food] != stock[item_to_make]
-        error_counter -= 1
+ 
+  stock.each_key do |key, value|  
+    stock.has_key?(item_to_make)
+    if false
+      raise ArgumentError.new("#{item_to_make} is not a valid input")
     end
   end
 
-  if error_counter != 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
-
-  serving_size = library[item_to_make]
+  serving_size = stock[item_to_make]
   serving_size_mod = order_quantity % serving_size
 
 if serving_size_mod == 0
     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
- else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+elsif serving_size_mod >= 1 
+    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: #{order_quantity - serving_size_mod} cookies"
   end
 end
-
 
 p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
