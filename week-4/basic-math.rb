e8b535bdd2e34328e22efe22dcc83b0762dd2564
Un Choi <umo2587@gmail.com>
2015-10-03 23:00:39 -0400

    add local variables and basic arithmetical expressions file

--- week-4/basic-math.rb
+++ week-4/basic-math.rb
@@ -1,18 +1,14 @@
 # Solution Below
-num1 = 99
-num2 = 55
+num1 = 78
+num2 = 34
 sum = num1 + num2
 difference = num1 - num2
 quotient = num1.to_f / num2.to_f
 product = num1 * num2
-modulus = num1.to_f % num2.to_f
-
-
-
+modulus = num1 % num2
 
 
 # RSpec Tests. They are included in this file because the local variables you are creating are not accessible across files. If we try to run these files as a separate file per normal operation, the local variable checks will return nil.
-
 
 describe 'num1' do
   it "is defined as a local variable" do
