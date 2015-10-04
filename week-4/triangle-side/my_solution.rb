41d81a163d4c2a11b200a60bdfa0a40ca6973305
Un Choi <umo2587@gmail.com>
2015-10-03 21:54:14 -0400

    add files for triangle side lengths

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -4,9 +4,10 @@
 # Your Solution Below
 
 def valid_triangle?(a, b, c)
- if a+b>c && b+c>a && a+c>b
-   true
- else
-   false
- end
+  if (a + b) > c && (b + c) > a && (a + c) > b
+    true
+  else 
+    false
+  end
+  
 end
