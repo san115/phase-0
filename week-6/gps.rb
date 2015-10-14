ca410b6e6c1e83a93856f0a511979d0a661086e3
Un Choi <umo2587@gmail.com>
2015-10-14 00:31:57 -0400

    add updated gps file

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,33 +1,41 @@
 # Your Names
-# 1)
-# 2)
+# 1) Un Choi
+# 2) Peter Wiebe
 
-# We spent [#] hours on this challenge.
+# We spent [1.25] hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, order_quantity)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
-
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
-    end
-  end
-
-  if error_counter > 0
+  # Library hash contains all baked goods that can be made
+  inventory = {"cookie" => 1, "cake" =>  5, "pie" => 7}
+#   # Check to see whether the item being requested is in the library
+#   error_counter = 3
+#   library.each do |food|
+#     if library[food] != library[item_to_make]
+#       p error_counter += -1
+#     end
+#   end
+  # If the item requested is not in the library then raise an error
+  if !inventory.has_key?(item_to_make)
     raise ArgumentError.new("#{item_to_make} is not a valid input")
   end
-
-  serving_size = library.values_at(item_to_make)[0]
-  serving_size_mod = order_quantity % serving_size
-
-  case serving_size_mod
-  when 0
+  # Match the item requested to the item in the library and calculate how many ingredients are left over
+  serving_size = inventory[item_to_make]
+  leftover = order_quantity % serving_size
+  baked_goods = ""
+  inventory.each do |key, value|
+    if value <= leftover && leftover > 0
+      baked_goods += " " + key + " " + (leftover/value).to_s
+    end
+  end
+  # Return the number of servings that can be made and how many left over ingredients are available for other items
+#   case serving_size_mod
+#   when 0
+  if leftover == 0
     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
   else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{leftover} leftover ingredients. Suggested baking items:#{baked_goods}"
   end
 end
 
@@ -39,5 +47,14 @@ p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
+
 #  Reflection
+# What did you learn about making code readable by working on this challenge? It helps to refactor so codes are brief and
+# concise. Another would be to name variables that helps to describe the tasks being performed.
+# Did you learn any new methods? What did you learn about them? I learned the has_key? and has_value? methods and that 
+# by adding an exclamation point infront of the variable when calling a method with a question mark, which gives boolean
+# values true or false, you can test for false statements.
+# What did you learn about accessing data in hashes? I learned to check for key and value of a hash through the has_key? or has_value? methods.
+# What concepts were solidified when working through this challenge? Refactoring is a useful way to improve the code and 
+# it's best to make the code clear and concise to make them readable.
 
