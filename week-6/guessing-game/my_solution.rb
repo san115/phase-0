1a3f862d09ae8517ca82c929bede7e892ea22289
Un Choi <umo2587@gmail.com>
2015-10-19 12:44:05 -0400

    add files for guessing game

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -18,8 +18,9 @@
 #   --true if solved; false if not solved
 # 
 
-# Initial Solution
 
+# Initial Solution
+# This code has some issues, but haven't figured out how to solved them.
 class GuessingGame
   def initialize(answer)
     @answer = answer
@@ -29,47 +30,47 @@ class GuessingGame
     @answer
   end
 
-  def Guessing_Game_guess(number)
-    @answer = 10
+  def guess(number)
+
     if number.to_i > @answer
       guess = :high
-      p guess
     elsif number.to_i < @answer
       guess = :low
-      p guess
     else number.to_i == @answer
       guess = :correct
-      p guess
     end
   end
     
-  def Guessing_Game_solved?(guess)
+  def solved?
+    guess = guess
     if guess == @answer
-      p true    
+     true    
     else  
-      p false
+     false
     end
   end
   
 end
 
 
-game = GuessingGame.new(10)
-game.Guessing_Game_guess(5)
-game.Guessing_Game_solved?(5)
-
-
-
 # Refactored Solution
-
-
-
-
-
+#This code still has some issues, so not yet at stage to refactor.
 
 # Reflection
+# How do instance variables and methods represent the characteristics and behaviors (actions) of 
+# a real-world object? Instance variables and methods follow an established plan to get work done,
+# which is similar to real-world objects, objects being a broad definition. Normally, real-life objects 
+# follow an established patterns based on set conditions, the established patterns can be likened to methods and instance 
+# variables could be likened to the set conditions.
+# When should you use instance variables? What do they do for you? Instance variables are useful as they can
+# be passed to any methods within a class. This is useful, since the variable only has to be defined once, insted
+# of being defined at each use.
+# Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what 
+# did you struggle with? As I understand it, flow control has to do with how information gets passed through operations, which can be
+# things like loops or if/else type of conditions, or variables that gets passed from method to method. I think I do have trouble with it, 
+# since I was not able to fix the code to access the latest attempt at the guess and return the result.
+# Why do you think this code requires you to return symbols? What are the benefits of using symbols? Symbols
+# are fixed values, so they do not change. This is useful to provide an accurate return for the results.
 
-# How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
-# When should you use instance variables? What do they do for you?
-# Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
-# Why do you think this code requires you to return symbols? What are the benefits of using symbols?
+
+
